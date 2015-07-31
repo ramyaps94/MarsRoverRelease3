0688430f063c8575634a9e116a61df76f6c0b227
ramyaps94 <ramya.sudarshan1994@gmail.com>
2015-07-31 12:03:37 +0530

    rover is within boundary

--- src/com/rover/Rover.java
+++ src/com/rover/Rover.java
@@ -4,12 +4,21 @@ public class Rover {
     private int xCoOrdinate;
     private int yCoOrdinate;
     private char direction;
+
     public Rover(int xCoOrdinate, int yCoOrdinate, char direction) {
         this.xCoOrdinate = xCoOrdinate;
         this.yCoOrdinate = yCoOrdinate;
         this.direction = direction;
     }
+
     public Rover getRoversPosition() {
         return this;
+    }
+
+    public Boolean IsRoverIsWithinThePlateau(Plateau plateau) {
+        if(xCoOrdinate < plateau.getTopRightXCoOrdinate() || xCoOrdinate > plateau.getBottomLeftXCoOrdinate()
+                && yCoOrdinate <plateau.getTopRightYCoOrdinate() || yCoOrdinate > plateau.getBottomLeftYCoOrdinate())
+            return true;
+        else return false;
     }
 }
--- test/com/rover/RoverTest.java
+++ test/com/rover/RoverTest.java
@@ -7,9 +7,17 @@ import static org.junit.Assert.assertEquals;
 public class RoverTest {
 
     @Test
-    public void shouldReturnThePositionOfTheRover() {
+    public void shouldKnowThePositionOfTheRover() {
         Rover rover = new Rover(1,2,'N');
 
         assertEquals(rover ,rover.getRoversPosition());
+    }
+
+    @Test
+    public void shouldKnowWhetherRoverIsWithinPlateau() {
+        Rover rover = new Rover(1,2,'N');
+        Plateau plateau = new Plateau(5, 5);
+
+        assertEquals(true ,rover.IsRoverIsWithinThePlateau(plateau));
     }
 }
