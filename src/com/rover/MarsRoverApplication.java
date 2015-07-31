ef9e7f4f5ad1262ba4a7fa6d4e4897a5c1bbae25
ramyaps94 <ramya.sudarshan1994@gmail.com>
2015-07-31 10:52:07 +0530

    |004| view is now able to accept the input

--- src/com/rover/MarsRoverApplication.java
+++ src/com/rover/MarsRoverApplication.java
@@ -4,5 +4,10 @@ public class MarsRoverApplication {
     public static void main(String[] args) {
         View view = new View();
         view.display("Enter the Boundaries of the Plateau");
+        String Input = view.acceptInput();
+        view.display("Enter the current position of the rover");
+        Input = view.acceptInput();
+        view.display("Enter the controlling string to move the rover");
+        Input = view.acceptInput();
     }
 }
--- src/com/rover/View.java
+++ src/com/rover/View.java
@@ -1,7 +1,14 @@
 package com.rover;
 
+import java.util.Scanner;
+
 public class View {
     public void display(String s) {
         System.out.println(s);
+    }
+
+    public String acceptInput() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
     }
 }
